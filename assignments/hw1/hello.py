8073b077f712f9416dfda4de3c0e9d3c033a3039
kristinamarina32 <rydbomka@g.cofc.edu>
2021-09-02 15:20:45 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Kristina Rydbom
+hello.py
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
