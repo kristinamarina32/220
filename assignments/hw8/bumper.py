d5367f3627c36486823373b7e71c43f2728ad85d
kristinamarina32 <rydbomka@g.cofc.edu>
2021-11-04 23:55:42 -0400

    bumper confused about methods

--- assignments/hw8/bumper.py
+++ assignments/hw8/bumper.py
@@ -2,35 +2,39 @@
 Name: Kristina Rydbom
 bumper.py
 
-Problem: HW 8  ---------
+Problem: HW 8 bumper assignment.
 
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 # """
 from graphics import *
 # from random import randint
+import random
 
 
 def main():
-    # window
+    # w, h, create window
     w = 700
     h = 500
 
     win = GraphWin("Bumper!", w, h)
+    # win.setBackground(color_rgb(255, 255, 255))
 
-    x1 = 300
-    y1 = 200
-    x2 = 500
-    y2 = 400
+    # radii and coordinates
 
-    point_a = Point(x1, y1)
-    point_b = Point(x2, y2)
+    radius1 = 40
+    radius2 = 40
 
-    ball = Circle(point_a, 40)
+    point_a = Point(300, 200)
+    point_b = Point(500, 400)
+
+    # creating circle 1!
+    ball = Circle(point_a, radius1)
     ball.setFill("purple")
     ball.setOutline("purple")
 
-    ball2 = Circle(point_b, 40)
+    # creating circle 2!
+    ball2 = Circle(point_b, radius2)
     ball2.setFill("green")
     ball2.setOutline("green")
 
@@ -38,18 +42,66 @@ def main():
     ball.draw(win)
     ball2.draw(win)
 
-    for i in range(500):
-        ball.move(-10, 0)
-        ball2.move(10, 0)
-        time.sleep(0.1)
-        # if y1 == 500 or y1 == 0:
-        #     ball.move(10, 0)
-        if (x1 == 700) or (x1 == 0):
-            ball.move(10, 0)
+    # move_amount1 = randint(start1, end1)
+    d1x = random.randint(0, 10)
+    d1y = random.randint(-10, -1)
+    d2x = random.randint(-10, -1)
+    d2y = random.randint(0, 10)
 
+    # for calculating distance
+    c1x = point_a.getX()
+    c1y = point_a.getY()
+    c2x = point_b.getX()
+    c2y = point_b.getY()
+    formula = ((c1x - c2x) ** 2 + (c1y - c2y) ** 2) ** (1 / 2)
+    distance = formula
 
-    win.getMouse()
-#
+    # while loop!
+    while not win.checkMouse():
+        # side to side
+        if (ball.getCenter().getX() < 660) and (ball.getCenter().getX() > 40):
+            ball.move(d1x, d1y)
+            time.sleep(0.02)
+        else:
+            ball.move(-d1x, d1y)
+            d1x = d1x * -1
+
+        if (ball2.getCenter().getX() < 660) and (ball2.getCenter().getX() > 40):
+            ball2.move(d2x, d2y)
+            time.sleep(0.02)
+        else:
+            ball2.move(-d2x, d2y)
+            d2x = d2x * -1
+
+        # up and down
+        if (ball.getCenter().getY() < 460) and (ball.getCenter().getY() > 40):
+            ball.move(d1x, d1y)
+            time.sleep(0.02)
+        else:
+            ball.move(d1x, -d1y)
+            d1y = d1y * -1
+
+        if (ball2.getCenter().getY() < 460) and (ball2.getCenter().getY() > 40):
+            ball2.move(d2x, d2y)
+            time.sleep(0.02)
+        else:
+            ball2.move(d2x, -d2y)
+            d2y = d2y * -1
+
+        # collision
+        if (radius1 + radius2) == distance:
+            ball.move(d1x, -d1y)
+            d1y = d1y * -1
+            ball2.move(-d2x, -d2y)
+
+        # other methods
+        # hit_vertical(), hit_horizontal(), get_random_color(), did_collide(Circle ball, Circle ball2)
+        # confused about what to do with these methods... ?
+        # if (ball.getCenter().getX() == 660) and (ball.getCenter().getX() == 40) and :
+        #     return True
+        # else:
+        #     return False
+
     win.getMouse()
 
     win.close()
